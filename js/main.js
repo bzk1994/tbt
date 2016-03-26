4e2ff4220afeb525085e673d57ecfdeffebfecc6
bzk1994 <1427249899@qq.com>
2016-03-27 00:51:16 +0800

    顶部右侧表单基本完成
    按钮尚未写完

--- js/main.js
+++ js/main.js
@@ -69,4 +69,70 @@ window.onload = function () {
 		}
 	}	
 	btn_right.onclick = right_lb;
+
+
+		// **************************banner条浮动样式设置
+	//引入添加删除指定类的方法
+
+	//hasClass:判断样式是否存在
+	function hasClass(obj, cls) {  
+	    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));  
+	}  
+	//addClass:为指定的dom元素添加样式
+	function addClass(obj, cls) {  
+    if (!hasClass(obj, cls)) obj.className += " " + cls;  
+	}  
+	//removeClass:删除指定dom元素的样式 
+	function removeClass(obj, cls) {  
+	    if (hasClass(obj, cls)) {  
+	        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');  
+	        obj.className = obj.className.replace(reg, ' ');  
+	    }  
+	}  
+
+	//为菜单设置浮动监听事件
+	// var lis = document.getElementsByClassName('has_secondMenu');
+	// for (var i = 0; i < lis.length; i++) {
+	// 	lis[i].onmouseover = function () {
+	// 		addClass(this,'menu_hover');
+	// 	}
+	// 	lis[i].onmouseout = function () {
+	// 		removeClass(this,'menu_hover');
+	// 	}
+	// }
+
+	// **********************************************************************************contain开始
+
+	//表单按钮
+	var lis = document.getElementsByClassName("con_tlBox_tab")[0].children[0].children;
+	//获得三角形
+	var b = document.getElementById('con_tlBox_b');
+	//获得输入框
+	var input = document.getElementById('sq_form').children[0];
+	input.focus();
+	for (var i = 0; i < lis.length; i++) {
+		lis[i].setAttribute('index',i);
+		lis[i].onclick = function(){
+			for (var j = 0; j < lis.length; j++) {
+				removeClass(lis[j],'oning');
+			}
+			addClass(this,'oning');
+			var index = this.getAttribute('index');
+			b.style.left = (26+index*75)+'px';
+			//获得表单title
+			var tits = document.getElementsByClassName('form_title')[0].children[0].children;
+			for (var k = 0; k < tits.length; k++) {
+				tits[k].style.display = 'none';
+			}
+			tits[index].style.display = 'block';
+			//表单获取焦点
+			input.focus();
+		}
+	}
+
+	//表单省市区见selec.js
+
+
 }
+
+
